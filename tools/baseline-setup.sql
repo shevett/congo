1c04f9fddce5344f41ee419dcd400247eb993967
Dave Shevett <shevett@pobox.com>
2014-02-20 16:44:12 -0500

    Cleaning up some old files.

--- tools/baseline-setup.sql
+++
@@ -1,4 +0,0 @@
-insert into reg_master values (1000,'Admin',old_password('changeme'),'Admin','User','',true,0,'');
-insert into con_detail (con_cid,con_name) values (0,'Meta-event');
-insert into config_properties values (1,'Administrator','0','boolean',false,0,'Administrator',0,'Global','');
-insert into reg_properties values (1000,1,'Administrator','1');
